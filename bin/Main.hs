2b544018baa95970386f550555f819ada5d89809
yamadapc <tacla.yamada@gmail.com>
2016-08-08 16:05:00 -0300

    Initial commit

---
+++ bin/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import           Hpack.Convert.API
+
+main :: IO ()
+main = runHpackConvertApi
---
+++ src/Hpack/Convert/API.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Hpack.Convert.API
+  where
+
+import           Control.Monad.IO.Class
+import           Data.Aeson (Value(..), object, (.=))
+import qualified Data.HashMap.Strict as HashMap
+import           Data.Maybe
+import           Data.Monoid ((<>))
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import           Hpack.Config
+import           Hpack.Convert
+import           Network.HTTP.Types.Status
+import           System.Environment
+import           Web.Spock
+
+curlExample :: Text -> Text
+curlExample host = "curl " <> host <> " -F \"cabalfile=@./hpack-convert-api.cabal\""
+
+runHpackConvertApi :: IO ()
+runHpackConvertApi = do
+    host <- (Text.pack . fromMaybe "localhost") <$> lookupEnv "HOST"
+    port <- (fromMaybe 3000 . fmap read) <$> lookupEnv "PORT"
+    let host' = host <> ":" <> Text.pack (show port)
+    runSpock port $ spockT id $ do
+        get "/" $ do
+            text $ Text.unlines [ "Available methods:"
+                                , "POST /"
+                                , "  Converts cabal files sent through the `cabalfile` field"
+                                , "  to a package.yaml"
+                                , "  Example:"
+                                , "  " <> curlExample host'
+                                ]
+        post "/" $ do
+            fs <- files
+            case HashMap.lookup "cabalfile" fs of
+                Nothing -> resError $ Text.unlines [ "Missing `cabalfile` parameter"
+                                                   , "Example:"
+                                                   , curlExample host'
+                                                   ]
+                Just UploadedFile{..} -> do
+                    !epkg <- liftIO (fromPackageDescriptionString <$> readFile uf_tempLocation)
+                    case epkg of
+                        Left err -> resError "Invalid `cabalfile` .cabal file"
+                        Right pkg -> bytes (encodePackage pkg)
+  where
+    resError e = do
+        setStatus status422
+        text e
